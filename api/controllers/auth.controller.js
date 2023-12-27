c215279875738f4e0cd9c5c6c8a06a2938a79016
Harshvardhan <shreeharshpb11@gmail.com>
2023-12-27 18:45:36 +0530

    create error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 // import bcryptjs from 'bcryptjs'
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {userName , email , password} = req.body;
     // const hashedPassword = bcryptjs.hashSync(password , 10)
     const newUser = new User({userName , email,password })
@@ -11,6 +11,6 @@ export const signup = async (req, res) => {
         await newUser.save()
         res.status(201).json("User Created Successfully")
     }catch(error){
-        res.status(500).json(error.message);
+        next(error)
     }
 }
