b2eeccd4e080cc9069ebac593d05e480e8209fb3
Harshvardhan <shreeharshpb11@gmail.com>
2023-12-27 15:55:33 +0530

    create mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongooese from 'mongoose';
+
+
 
 const app = express();
+
+
+mongooese.connect("mongodb://localhost:27017/mern-estate", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(() => {
+    console.log("Connected MongoDB")
+}).catch(
+    console.log("Connection Falied")
+)
 
 app.listen(3000 , () =>{
     console.log("Server is running on port 3000");
