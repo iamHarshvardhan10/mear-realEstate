041873e5dd7cea2bc6d191a1c37681aa9d990c8b
Harshvardhan <shreeharshpb11@gmail.com>
2023-12-27 16:46:18 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-
+import userRouter from './routes/user.route.js';
 
 const app = express();
 
@@ -19,3 +19,5 @@ app.listen(3000 , () =>{
     console.log("Server is running on port 3000");
 });
 
+
+app.use('/api/user/',userRouter)
