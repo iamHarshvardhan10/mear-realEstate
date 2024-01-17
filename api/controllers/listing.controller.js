1bcaf046da7e1eb85068bfb9e7d72893c22749ab
Harshvardhan <shreeharshpb11@gmail.com>
2024-01-17 19:52:20 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -29,3 +29,27 @@ export const deleteListing = async (req,res,next) => {
         next(error)
     }
 }
+
+
+
+export const updateListing = async (req,res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404,"Listing not found"))
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, "Unauthorized Access!"))
+    }
+
+    try {
+        const updateListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true }
+        );
+        res.status(200).json(updateListing)
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { createListing , deleteListing} from '../controllers/listing.controller.js'
-import { verifyToken } from '../utils/verifyUser.js'
+import { verifyToken , updateListing } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
 router.post('/create',verifyToken, createListing)
 router.delete('/delete/:id', verifyToken , deleteListing )
-
+router.post('/update/:id', verifyToken , updateListing)
 
 
 
